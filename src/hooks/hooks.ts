bed0b8a2d3cf1114d4279a6f863eb0cf8f3eecaf
Patryk-Polanski <pat.polanski@gmail.com>
2024-02-01 20:48:22 +0000

    part 2: create more type safe useSelector function

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,6 +1,13 @@
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../store/store';
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux';
+import { AppDispatch, RootState } from '../store/store';
 
 type DispatchFunction = () => AppDispatch;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/* YOu don't need to recreate these hooks for each of your store slices, You only create them once - hence you might also want to call them useAppDispatch and useAppSelector */
